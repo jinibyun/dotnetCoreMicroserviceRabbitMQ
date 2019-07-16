cc9c79935795b850c43d4c90b615652793729a4c
Jini Byun <jinibyun@gmail.com>
2019-07-16 00:31:43 -0400

    commit

---
+++ Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            // connect RabbitMQ and send
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string message = "Getting started with .net core rabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Sent message {0} ...", message);
+            }
+            Console.WriteLine("Press [enter] to exit the Sender app...");
+            Console.ReadLine();
+        }
+    }
+}
